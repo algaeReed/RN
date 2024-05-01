03acd3d306b9d88ffbfa7d8850dc7e28dbb594af
张哲 <jefferylong@163.com>
2024-05-01 22:07:23 +0800

    feat: add react native navigation

---
+++ HelloWord.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {
+	StyleSheet,
+	Text,
+	View,
+} from 'react-native';
+
+function HelloWord(): React.JSX.Element {
+	return (
+		<View>
+			<Text>HelloWord</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+});
+
+export default HelloWord;
